ecd4683ec4f958a578b1995d7bcaff748677ef93
zilbertamar@gmail.com <zilbertamar@gmail.com>
2024-01-03 19:20:49 +0200

    Add project files.

---
+++ Zoo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Zoo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            //string s = "aaaaaaaaaa";
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    s = s + s;
+            //}
+            var s1 = new StringBuilder();
+            for (int i = 0; i < 1000000000000; i++)
+            {
+                s1 = s1.Append(s1);
+            }
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
